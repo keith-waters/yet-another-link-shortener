52156f74ffaeaec50bdf7c420483509c33b369b1
keith-waters <readingwaters@gmail.com>
2022-09-07 12:44:45 -0400

    Add content and some styling to the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Copyright from '../src/Copyright';
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="sm">
       <Box
         sx={{
           my: 4,
@@ -18,31 +18,96 @@ const Home: NextPage = () => {
           alignItems: 'center',
         }}
       >
-        <Typography variant="h4" component="h1" gutterBottom>
-          Yet Another Link Shortener!
+        <Typography variant="h2" component="h1" gutterBottom>
+          <strong>Yet Another Link Shortener!</strong>
         </Typography>
-        <Typography component="p">
-					Turn your long links into short links.
+
+        <Typography variant="h3" component="h2">
+					Turn your long links into short links
         </Typography>
-        <Typography component="p">
-					This: https://readingwaters.com/notes/yals-technology-choices-and-system-layout	
+				<Box
+					sx={{
+						my: 4,
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<img
+						src={`/rulers.jpg`}
+						alt='Rulers'
+						loading="lazy"
+						width='150px'
+						style={{borderRadius: "10px", margin: "10px"}}
+					/>
+					<Typography component="p">
+						This: <i>"https://readingwaters.com/notes/yals-technology-choices-and-system-layout"</i> <br/>turns into this: <i>"yals.to/yals-tech"</i>
+					</Typography>
+				</Box>
+
+        <Typography variant="h3" component="h2">
+					See all your short links in one place
         </Typography>
-        <Typography component="p">
-					turns into
+				<Box
+					sx={{
+						my: 4,
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<img
+						src={`/dashboard-mockup.png`}
+						alt='User dashboard mockup'
+						loading="lazy"
+						width='350px'
+						style={{borderRadius: "10px", margin: "10px"}}
+					/>
+					<Typography component="p">
+						In the dashboard you'll be able to manage all your links at the click of a button. You can create, edit, and delete everything in one place.
+					</Typography>
+				</Box>
+
+				<Typography variant="h3" component="h2">
+					Work with this guy
+				</Typography>
+				<Box
+					sx={{
+						my: 4,
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<img
+						src={`/keith-headshot-2.jpg`}
+						alt='Headshot of Keith'
+						loading="lazy"
+						height='200px'
+						style={{borderRadius: "10px", margin: "10px"}}
+					/>
+					<Typography 
+						component="p"
+						style={{margin: "10px"}}
+					>
+						Hi, I'm Keith. I'm that guy :), and the only person that works on YALS currently. So, any ideas, questions, comments, or concerns go directly to me.
+					</Typography>
+				</Box>
+
+        <Typography variant="h3" component="h2">
+					Sign up for updates
         </Typography>
-        <Typography component="p">
-					This: yals.to/yals-tech
-        </Typography>
-        <Typography component="p">
-					Interested? Sign up for updates!
-        </Typography>
-				<TextField name='First Name'/>
-				<TextField name='Last Name'/>
-				<TextField name='Email'/>
+				<TextField label='First Name'/>
+				<TextField label='Last Name'/>
+				<TextField label='Email'/>
 				<Button type='submit'>Sign me up!</Button>
         <Copyright />
       </Box>
     </Container>
+
   );
 };
 
