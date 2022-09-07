550ae7e5f709becc8f4798dad047b016f72a0267
keith-waters <readingwaters@gmail.com>
2022-09-07 16:25:09 -0400

    Install Plausible for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import Head from 'next/head';
+import PlausibleProvider from 'next-plausible'
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -17,15 +17,17 @@ interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+		<PlausibleProvider domain={process.env.NEXT_DOMAIN}>
+			<CacheProvider value={emotionCache}>
+				<Head>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+				</Head>
+				<ThemeProvider theme={theme}>
+					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</CacheProvider>
+		</PlausibleProvider>
   );
 }
