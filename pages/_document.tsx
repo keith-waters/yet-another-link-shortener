9d3dce338d7c10bca0a1906b3a268f240b1f1c91
keith-waters <readingwaters@gmail.com>
2022-09-12 16:46:04 -0400

    Add YALS logo and use App Bar for the header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import theme from '../src/theme';
@@ -11,7 +10,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/yals.png" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,21 +1,23 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
   return (
-    <Box
-      sx={{
-        height: "50px",
-        borderRadius: "5px",
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <Box sx={{ flexGrow: "1" }} />
-      <Box>
-        <Button>Sign in</Button>
-        <Button variant="contained">Sign up</Button>
-      </Box>
-    </Box>
+    <AppBar position="fixed">
+      <Toolbar>
+				<Box sx={{ flexGrow: 1 }}>
+					<img src="/yals.png" height="50" width="50" style={{borderRadius: "3px"}}/>
+				</Box>
+        <Button color="inherit">Sign in</Button>
+        <Button color="inherit">Sign up</Button>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
